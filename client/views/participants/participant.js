4b553959c853469898c9d7f47bec45dbbd3f2a7d
David Bimmler <bimmlerd@student.ethz.ch>
2015-02-22 12:11:13 +0100

    redesign navbar

--- client/views/participants/participant.js
+++ client/views/participants/participant.js
@@ -1,5 +1,9 @@
 Template.participant.helpers({
-        user: function() {
-        	return Meteor.users.findOne({_id: this.userId});
+        displayName: function() {
+        	return Meteor.users.findOne({_id: this.userId}).displayName;
+    	},
+    	weightString: function() {
+    		var w = Meteor.users.findOne({_id: this.userId}).weights;
+    		return w.five + " 5er, " + w.two + " 2er and " + w.one + " 1er"
     	}
 });
