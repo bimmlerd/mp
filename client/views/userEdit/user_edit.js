ba5304e9fa560f487bde5acd003fa51365ae2c58
David Bimmler <bimmlerd@student.ethz.ch>
2015-03-17 14:45:33 +0100

    rework user edit input verification

--- client/views/userEdit/user_edit.js
+++ client/views/userEdit/user_edit.js
@@ -1,34 +1,34 @@
 Template.userEdit.events({
 	'click :submit': function(e) {
 		e.preventDefault();
-		var err = false;
-		var form = {
-			dpName: $(e.target).parents().find('[name=displayName]').val(),
-			weights: {
-				five: $(e.target).parents().find('[name=five]').val(),
-				two: $(e.target).parents().find('[name=two]').val(),
-				one: $(e.target).parents().find('[name=one]').val()
-			}
-		}
+		// valid weights "type" for check
+		ValidWeight = Match.Where(function (x) {
+		  check(x, Number);
+		  return x >= 0 && x <= 4;
+		});
 
-
-		if ($.trim(form.dpName) === "") {
-			err = true;
-			Router.go('spevsList');
-		}
-
-		// FIXME reimplement with check(weight, Number)
-		for (var key in form.weights) {
-			if (form.weights.hasOwnProperty(key)) {
-				if (isNaN(parseInt(form.weights[key])) ||
-					form.weights[key] === "") {
-					form.weights[key] = undefined;
-					err = true;
-					Router.go('spevsList');
+		try {
+			var form = {
+				dpName: $(e.target).parents().find('[name=displayName]').val(),
+				weights: {
+					five: $(e.target).parents().find('[name=five]').val(),
+					two: $(e.target).parents().find('[name=two]').val(),
+					one: $(e.target).parents().find('[name=one]').val()
 				}
 			}
-		}
-		if (! err) {
+
+			if ($.trim(form.dpName) === "") {
+				throw new Meteor.Error("Invalid Data", "Can't have an empty name.");
+				Router.go('spevsList');
+			}
+
+
+			for (var key in form.weights) {
+				if (form.weights.hasOwnProperty(key)) {
+					check(parseInt(form.weights[key]), ValidWeight);
+				}
+			}
+
 			Meteor.users.update({"_id": Meteor.userId()},
 				{$set: {weights: form.weights, displayName: form.dpName}},
 				function(error) {
@@ -37,7 +37,11 @@ Template.userEdit.events({
 					} else {
 						Router.go('spevsList');
 					}
-				});
+				}
+			);
+
+		} catch(e) {
+			Router.go('spevsList');
 		}
 	}
 });
