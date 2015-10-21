00b5959eac118cb33c710a24a576b84f331c01a7
David Bimmler <bimmlerd@student.ethz.ch>
2015-10-21 14:17:52 +0200

    pre update working

--- client/views/integration/integration_login.js
+++ client/views/integration/integration_login.js
@@ -2,11 +2,6 @@ Template.integrationLogin.events({
 	"click #integration": function(e, t) {
 		e.preventDefault();
 		var success = Meteor.call("perform-integration", t.data.token);
-		if (!success) {
-			alert("Sorry, that didn't work. Try to start the process again from the bot.")
-			Router.go("/");
-			return;
-		}
 		ga('send', 'event', 'Integration', 'Telegram ID linked to Account');
 		Router.go("/");
 	}
